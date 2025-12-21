ac682c32e4c098b9b4880026f905f8f2f90390ea
LpMashinini <lebogangp84@gmail.com>
2025-12-21 14:28:22 +0200

    Add inputs and styled the appointment page

---
+++ Frontend/Frontend/src/Components/SPA/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Body from "./Body"
+import Testimonials from "./Testimonials"
+import Footer from "./Footer_Component"
+
+const Home = () => {
+  return (
+    <div>
+    <Header/>
+    <Body/>
+    <Testimonials/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ Frontend/Frontend/src/Components/SPA/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+
+  return (
+    <div className="nav-container">
+      <h1 className="logo">Blissful <span>&</span> Balance</h1>
+      <ul>
+        <li> <a href="#">Home</a></li>
+        <li> <a href="#">Service</a></li>
+        <li><a href="#">About us</a></li>
+        <Link to="/contact"  className="contact"><li>Contact</li> </Link>
+        <Link to="/appointment" className="appointment"><li>appointment</li></Link>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
