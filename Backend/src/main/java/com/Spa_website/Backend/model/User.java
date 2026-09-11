421a6d18951c66dd27fc8b37e4201b8beffe4266
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-12 00:49:38 +0200

    fix database connection

--- Backend/src/main/java/com/Spa_website/Backend/model/User.java
+++ Backend/src/main/java/com/Spa_website/Backend/model/User.java
@@ -1,8 +1,6 @@
 package com.Spa_website.Backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Setter
@@ -14,12 +12,25 @@ import lombok.*;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "first_name", nullable = false, length = 255)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 255)
     private String lastName;
+
+    @Column( name = "_email", nullable = false, length = 255)
     private String email;
+
+    @Column(name = "_password", nullable = false, length = 255)
     private String password;
+
+    @Column(name = "_confirm_password", nullable = false, length = 255)
     private String confirmPassword;
+
+    @Column(name = "_role", nullable = false, length = 255)
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
