adaab13d7f34f5ffecc101364ecceaf4a7d57152
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-12 04:13:15 +0200

    fix workflow error

--- Backend/src/main/java/com/Spa_website/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/Spa_website/Backend/config/SecurityConfig.java
@@ -7,15 +7,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
