ee18b5ef1e0052227a3aa18847c1171de233f0fb
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-15 01:10:16 +0200

    add jwt authentication

---
+++ Backend/src/main/java/com/Spa_website/Backend/jwtAuth/JwtUtil.java
@@ -0,0 +1,73 @@
+package com.Spa_website.Backend.jwtAuth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secretkey}")
+    private String secret;
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String createToken(String subject){
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 100 * 60 * 60 * 10))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return createToken(userDetails.getUsername());
+    }
+
+    private Boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims extractAllClaims(String token){
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private <T> T extractClaims(String token, Function<Claims,T> claimsResolver){
+
+        final Claims claims = extractAllClaims(token);
+        return (T) claimsResolver.apply(claims);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+}
--- Backend/src/main/java/com/Spa_website/Backend/repository/UserRepository.java
+++ Backend/src/main/java/com/Spa_website/Backend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.Spa_website.Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
