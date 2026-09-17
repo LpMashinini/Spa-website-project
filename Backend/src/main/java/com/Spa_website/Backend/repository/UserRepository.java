e493ee3b17520230204efa3402ba94b67e1b795b
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-17 16:38:55 +0200

    add optinal methodon Userrepository

--- Backend/src/main/java/com/Spa_website/Backend/repository/UserRepository.java
+++ Backend/src/main/java/com/Spa_website/Backend/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.Spa_website.Backend.repository;
 import com.Spa_website.Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumber(String number);
+    Boolean existsByEmail(String email);
+    Boolean existsByPhoneNumber(String number);
 }
