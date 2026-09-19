cb4f88ba48864b2c6054bddf5a174f3d09488ef0
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-19 14:46:57 +0200

    add email verification method

--- Backend/src/main/java/com/Spa_website/Backend/service/UserService.java
+++ Backend/src/main/java/com/Spa_website/Backend/service/UserService.java
@@ -1,10 +1,13 @@
 package com.Spa_website.Backend.service;
 
 import com.Spa_website.Backend.dto.UserRegistrationRequest;
+import com.Spa_website.Backend.model.Otp;
+import com.Spa_website.Backend.model.OtpType;
 import com.Spa_website.Backend.model.Role;
 import com.Spa_website.Backend.model.User;
 import com.Spa_website.Backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -33,4 +36,14 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    public void initiateEmailVerification(String email){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Otp otp = otpService.createOtp(user, OtpType.EMAIL_VERIFICATION);
+        emailService.sendOtpEmail(user.getEmail(), otp.getCode());
+    }
+
+
 }
