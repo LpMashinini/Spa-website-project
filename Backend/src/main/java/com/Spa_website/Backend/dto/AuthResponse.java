92480cbb740e7942f86a9db22eded54a832bff6f
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-19 13:24:37 +0200

    add AuthResponse DTO for token assignin

---
+++ Backend/src/main/java/com/Spa_website/Backend/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.Spa_website.Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String email;
+    private boolean verified;
+}
