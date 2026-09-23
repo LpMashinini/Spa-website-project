6cd217ca0686227d4715e22b547641b7072b364f
LpMashinini <lebogangmatshinini@gmail.com>
2026-09-24 01:38:57 +0200

    add create appointment endpoint in appointment controller

--- Backend/src/main/java/com/Spa_website/Backend/controller/AppointmentController.java
+++ Backend/src/main/java/com/Spa_website/Backend/controller/AppointmentController.java
@@ -1,4 +1,29 @@
 package com.Spa_website.Backend.controller;
 
+import com.Spa_website.Backend.dto.CreateAppointmentRequest;
+import com.Spa_website.Backend.model.Appointment;
+import com.Spa_website.Backend.service.AppointmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/appointment")
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<Appointment> createAppointment(@Valid @RequestBody CreateAppointmentRequest request){
+
+        Appointment appointment=  appointmentService.createAppointment(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointment);
+    }
 }
